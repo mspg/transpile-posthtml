5a03c2ec0012b1247eb060283a53756bb10765fd
Jascha <jascha@jaeh.at>
2018-11-16 05:20:24 +0100

    add src

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const path = require('path')
+const util = require('util')
+
+const posthtml = require('posthtml')
+const posthtmlExtend = require('posthtml-extend')
+const posthtmlInclude = require('posthtml-include')
+const posthtmlExpressions = require('posthtml-expressions')
+const minify = require('./minify')
+
+const is = require('@magic/types')
+const log = require('@magic/log')
+
+const exists = util.promisify(fs.exists)
+
+const POST_HTML = async ({ buffer, config }) => {
+  const { ENV } = config
+  config.HTML_DIR = config.HTML_DIR || '/'
+
+  config.WEB_ROOT = ENV === 'production' && config.WEB_ROOT
+    ? config.WEB_ROOT
+    : '/'
+
+  try {
+    if (is.empty(buffer)) {
+      throw new Error('POST_HTML: Missing argument: { buffer } missing in first argument')
+    }
+
+    if (!is.string(buffer)) {
+      if (is.buffer(buffer)) {
+        buffer = buffer.toString()
+      } else {
+        throw new Error('POST_HTML: buffer has to be a string or buffer')
+      }
+    }
+
+    const plugins = [
+      posthtmlExtend({ root: config.HTML_DIR }),
+      posthtmlInclude({ root: config.HTML_DIR }),
+    ]
+
+    const varFilePath = path.join(config.HTML_DIR, 'variables.js')
+    let locals = config
+    if (await exists(varFilePath)) {
+      locals = {
+        ...locals,
+        ...require(varFilePath),
+      }
+    }
+    plugins.push(posthtmlExpressions({ locals }))
+    
+    const html = await posthtml(plugins).process(buffer/*, options */)
+    const minified = minify(html.html)
+    return minified
+  }
+  catch(e) {
+    throw e
+  }
+}
+
+module.exports = POST_HTML
---
+++ src/minify.js
@@ -0,0 +1,8 @@
+module.exports = html => 
+    html
+    // replace newlines after tags with nothin
+    .replace(/\>\n/gim, '>')
+    // replace all whitespaces with one space per whitespace group (\n\t\n === ' ').
+    .replace(/\s\s+/gim, ' ')
+    // replace spaces beween tags or in empty tags with nothing
+    .replace(/> </gim, '><')
