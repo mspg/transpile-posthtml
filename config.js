d05194fdf0e372abcf65b28117f76c4e2201d634
Jascha <jascha@jaeh.at>
2018-11-16 05:20:44 +0100

    add config and example

---
+++ config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const HTML = require('./src')
+
+module.exports = {
+  TRANSPILERS: {
+    HTML,
+  },
+  // files get loaded from example/src and example/includes
+  CWD: path.join(__dirname, 'example'),
+  // and published in example/publish
+  OUT_DIR: path.join(__dirname, 'example', 'public'),
+  // web root of the github page
+  WEB_ROOT: 'https://mspg.github.io/transpile-posthtml/',
+}
