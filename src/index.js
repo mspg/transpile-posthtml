05b6de981611ba858ac747d8d013b639d9402013
Jascha <jascha@jaeh.at>
2018-11-18 11:31:40 +0100

    add mixins

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const posthtml = require('posthtml')
 const posthtmlExtend = require('posthtml-extend')
 const posthtmlInclude = require('posthtml-include')
 const posthtmlExpressions = require('posthtml-expressions')
+const posthtmlMixins = require('posthtml-mixins')
 
 const is = require('@magic/types')
 const log = require('@magic/log')
 
 const exists = util.promisify(fs.exists)
+const readFile = util.promisify(fs.readFile)
 
 const POST_HTML = async ({ buffer, config }) => {
   const { ENV } = config
@@ -34,6 +36,7 @@ const POST_HTML = async ({ buffer, config }) => {
     const plugins = [
       posthtmlExtend({ root: config.HTML_DIR }),
       posthtmlInclude({ root: config.HTML_DIR }),
+      posthtmlMixins(),
     ]
 
     const varFilePath = path.join(config.HTML_DIR, 'variables.js')
@@ -46,6 +49,17 @@ const POST_HTML = async ({ buffer, config }) => {
     }
     plugins.push(posthtmlExpressions({ locals }))
 
+    const mixinFile = path.join(config.HTML_DIR, 'mixins.html')
+    if (await exists(mixinFile)) {
+      const mixinString = await readFile(mixinFile, 'utf8')
+      buffer = mixinString + buffer
+    }
+    const globalMixinFile = path.join(__dirname, 'mixins.html')
+    if (await exists(globalMixinFile)) {
+      const globalMixinString = await readFile(globalMixinFile, 'utf8')
+      buffer = globalMixinString + buffer
+    }
+  
     const html = await posthtml(plugins).process(buffer /*, options */)
     return html.html
   } catch (e) {
