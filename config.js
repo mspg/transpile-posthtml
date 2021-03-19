9b3455f6a80025b0dc8e4d3fb150d15049c3eb2b
jaeh <jascha@jaeh.at>
2021-03-19 02:35:10 +0100

    config: use cwd instead of __dirname

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 import path from 'path'
 import html from './src/index.js'
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const cwd = process.cwd()
 
 export default {
   TRANSPILERS: {
     html,
   },
   // files get loaded from example/src and example/includes
-  CWD: path.join(__dirname, 'example'),
+  CWD: path.join(cwd, 'example'),
   // and published in example/publish
-  OUT_DIR: path.join(__dirname, 'example', 'public'),
+  OUT_DIR: path.join(cwd, 'example', 'public'),
   // web root of the github page
   WEB_ROOT: 'https://mspg.github.io/transpile-posthtml/',
   LINT: {
